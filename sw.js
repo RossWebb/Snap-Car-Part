3841ecc8eacf046b6332d9a37bbf26532f3d6e2a
RossWebb <rnlwebb@gmail.com>
2026-05-22 16:20:33 +1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = 'partsnap-v1';
+
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
