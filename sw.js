b4629726aaf4661fb5eef226b2640cf3518e09e3
RossWebb <rnlwebb@gmail.com>
2026-05-24 16:49:35 +1000

    Update service worker cache version to v1.6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'partsnap-v1.5';
+const CACHE = 'partsnap-v1.6';
 
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(clients.claim()));
