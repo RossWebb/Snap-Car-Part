0fc3b2abd527eb3fbca322daf03701f7e82563d1
RossWebb <rnlwebb@gmail.com>
2026-05-26 11:23:08 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,11 @@
 const CACHE = 'partsnap-v1.9';
 
 self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+self.addEventListener('activate', e => e.waitUntil(
+  caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => clients.claim())
+));
 self.addEventListener('fetch', e => {
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
